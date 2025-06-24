9bac88abdaf53e917dda75cafb91f669a1a8df57
padmajapinnika <padmajapinnika@gmail.com>
2025-06-24 11:24:22 -0400

    Update ALLOWED_HOSTS with site and development server URL

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['enigmatic-river-86724-cf59f6c0f42b.herokuapp.com']
 
 
 # Application definition
